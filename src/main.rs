5016444e1cb41da710d58d524552bd8a8f82cdd7
Linfeng Qian <thewawar@gmail.com>
2021-06-27 11:58:27 +0800

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// #![no_std]
+// #![no_main]
+// #![feature(lang_items)]
+// #![feature(alloc_error_handler)]
+// #![feature(panic_info_message)]
+
+// use ckb_std::default_alloc;
+
+// ckb_std::entry!(program_entry);
+// default_alloc!();
+
+fn main() {
+    println!("Hello, world!");
+}
