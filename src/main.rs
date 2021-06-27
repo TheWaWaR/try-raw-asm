240ebbbf68b3d682ff67c0c079b4b5a43d079518
Linfeng Qian <thewawar@gmail.com>
2021-06-27 15:52:07 +0800

    Test .byte directive

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,26 @@
 // ckb_std::entry!(program_entry);
 // default_alloc!();
 
+#![feature(asm)]
+
 fn main() {
-    println!("Hello, world!");
+    let a = 234;
+    let b = 333;
+    println!("add({}, {}) => {}", a, b, asm_add(a, b));
+}
+
+#[no_mangle]
+pub extern "C" fn asm_add(a: i32, b: i32) -> i32 {
+    let c: i32;
+    unsafe {
+        asm!(
+            ".byte 0x90",
+            "add {1}, {2}",
+            "mov {0}, {1}",
+            out(reg) c,
+            in(reg) a,
+            in(reg) b,
+        );
+    }
+    c
 }
