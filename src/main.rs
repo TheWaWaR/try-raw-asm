e00cf77960e4cfdfce1964c3700e864eea1c11a3
Qian Linfeng <thewawar@gmail.com>
2021-10-23 11:56:47 +0800

    Add encoding show case

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,17 @@ fn raw_asm_add(a: i64, b: i64) -> i64 {
     let c: i64;
     // $ riscv64-unknown-elf-objdump -d target/riscv64imac-unknown-none-elf/release/deps/try_raw_asm-5cc3a156705d970b > xxx.s
     // In xxx.s
-    // 11af4:	00b50633          	add	a2,a0,a1
+    // 11af4:	00b50633          	add	a2,a0,a1 (add x12,x10,x11) (12: 01100, 10: 01010, 11: 01011)
     unsafe {
+        // add rd, rs1, rs2
+        // 0000000, rs2, rs1, 000, rd, 0110011, ADD
+        // 0000000, 01011, 01010, 000, 01100, 0110011, ADD
+        // =========================
+        // | 00000000 |   0 | 0x00 |
+        // | 10110101 | 181 | 0xb5 |
+        // | 00000110 |   6 | 0x06 |
+        // | 00110011 |  51 | 0x33 |
+        // =========================
         asm!(
             ".byte 0x33, 0x06, 0xb5, 0x00 \n /* {0}, {1}, {2} */",
             out(reg) c,
